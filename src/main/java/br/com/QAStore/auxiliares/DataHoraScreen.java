be5e34130c53e571104e57d4779a9229ae68ae38
kamila.albuquerque <kamila.albuquerque@KAMILA-NOTE>
2023-02-18 15:54:25 -0300

    Classes auxiliares criadas

---
+++ src/main/java/br/com/QAStore/auxiliares/DataHoraScreen.java
@@ -0,0 +1,16 @@
+package br.com.QAStore.auxiliares;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+//Calse que formata como a data e hora ficará visivel no nome do arquivo de screen e folder
+public class DataHoraScreen {
+
+	public static String dataHoraArquivo() {
+		
+		Timestamp ts = new Timestamp(System.currentTimeMillis());
+		
+		return new SimpleDateFormat("yyyy_MM_hh_mm_ss").format(ts);
+	}
+
+}
---
+++ src/main/java/br/com/QAStore/auxiliares/Diretorio.java
@@ -0,0 +1,24 @@
+package br.com.QAStore.auxiliares;
+
+import java.io.File;
+import java.io.IOException;
+
+//Classe que está configurado como será criado o diretorio para salvar as evidencias
+public class Diretorio {
+	
+	public static String nomePasta;
+
+	public static void criaDiretorio() throws IOException {
+		// Cria Diretorio para cada vez que o teste é executado
+		try {
+			nomePasta = "Evidência" +  DataHoraScreen.dataHoraArquivo();
+			File pasta = new File(System.getProperty("user.dir") +"\\target\\Screenshot\\" +"//"+ nomePasta);
+			pasta.mkdir();
+			 
+		}
+		catch(Exception f) {
+			System.out.println("Erro ao criar pasta " + f.getMessage());
+		}
+			
+	}
+}
